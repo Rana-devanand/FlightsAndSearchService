2f17e3455e0cd1d35ba5f658b5976168e5344530
Devanand kr Rana <gdsc.devanandrana@gmail.com>
2024-07-02 12:57:21 +0530

    Adding a middleware Body-Parser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
 
 const setupAndStartService = async () => {
+  // Create the Server Object
   const app = express();
 
+  // Create the middleWares
+  app.use(bodyParser.json());
+  app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  );
+
+  // PORT setup where our Machine is in Running mode..
   app.listen(PORT, () => {
     console.log(`Server Started at ${PORT}`);
   });
